7132b6d1981ee8ffc21feaee94f1543326697004
Maruti Gollapudi <maruti.gollapudi@gmail.com>
2019-10-30 14:52:06 +0530

    initial version

---
+++ HeroRec.js
@@ -0,0 +1,123 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import {
+  ViroARScene,
+  ViroMaterials,
+  ViroNode,
+  ViroAnimations,
+  Viro3DObject,
+  ViroLightingEnvironment,
+  ViroARImageMarker,
+  ViroARTrackingTargets,
+  ViroSphere,
+  ViroSpotLight,
+  ViroQuad,
+  ViroText
+} from 'react-viro';
+
+var createReactClass = require('create-react-class');
+
+
+var HeroRec = createReactClass({
+  getInitialState() {
+    return {
+      texture: "white",
+      playAnim: false,
+      animateCar: false,
+      tapWhite: false,
+      tapBlue: false,
+      tapGrey: false,
+      tapRed: false,
+      tapYellow: false,
+    }
+  },
+
+  render: function() {
+    return (
+      <ViroARScene>
+
+        <ViroLightingEnvironment source={require('./res/tesla/garage_1k.hdr')}/>
+
+        <ViroARImageMarker target={"logo"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
+          <ViroText
+            text="Captain Captain"
+            textAlign="left"
+            textAlignVertical="top"
+            textLineBreakMode="justify"
+            textClipMode="clipToBounds"
+            color="#ff0000"
+            width={2} height={2}
+            style={{fontFamily:"Arial", fontSize:20, fontWeight:400, fontStyle:"italic", color:"#0000FF"}}
+            position={[0,0,-5]}
+          />
+        </ViroARImageMarker>
+      </ViroARScene>
+    );
+  },
+  _onAnchorFound() {
+    this.setState({
+      animateCar: true,
+    })
+  },
+  _toggleButtons() {
+    this.setState({
+      animName: (this.state.animName == "scaleUp" ? "scaleDown" : "scaleUp"),
+      playAnim: true
+    })
+  },
+  _selectWhite(){
+    this.setState({
+      texture : "white",
+      tapWhite: true
+    })
+  },
+  _selectBlue(){
+    this.setState({
+      texture : "blue",
+      tapBlue: true
+    })
+  },
+  _selectGrey(){
+    this.setState({
+      texture : "grey",
+      tapGrey: true
+    })
+  },
+  _selectRed(){
+    this.setState({
+      texture : "red",
+      tapRed: true
+    })
+  },
+  _selectYellow(){
+    this.setState({
+      texture : "yellow",
+      tapYellow: true
+    })
+  },
+  _animateFinished(){
+    this.setState({
+      tapWhite: false,
+      tapBlue: false,
+      tapGrey: false,
+      tapRed: false,
+      tapYellow: false,
+    })
+  },
+});
+
+ViroARTrackingTargets.createTargets({
+  logo : {
+    source : require('./res/CapAmerica.jpg'),
+    orientation : "Up"
+    // physicalWidth : 0.165 // real world width in meters
+  }
+});
+
+
+
+module.exports = HeroRec;
---
+++ index.js
@@ -0,0 +1,8 @@
+import { AppRegistry } from 'react-native';
+import App from './App.js';
+import HeroRec from './HeroRec'
+
+AppRegistry.registerComponent('APP_NAME_HERE', () => App);
+
+// The below line is necessary for use with the TestBed App
+AppRegistry.registerComponent('ViroSample', () => HeroRec);
